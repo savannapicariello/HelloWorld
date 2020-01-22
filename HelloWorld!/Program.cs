49447b208b8db96aca591aefedff6232e348a356
Savanna Picariello <savannapicariello@gmail.com>
2020-01-22 13:54:23 -0500

    Add project files.

---
+++ HelloWorld!/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
